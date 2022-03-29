daeb7af953969bbc4b28654525aa040105b2258e
Aleksandar Dimitrov <alexkdimitrov@gmail.com>
2022-03-29 05:58:17 +0300

    Added Inheritance - Lab

---
+++ 01.Inheritance-Lab/T04.RandomList/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList list = new RandomList() { "aaa", "bbb", "ccc" };
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(string.Join(", ", list));
+        }
+    }
+}
---
+++ 01.Inheritance-Lab/T05.StackOfStrings/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stackOfStrings = new StackOfStrings();
+            Console.WriteLine(stackOfStrings.IsEmpty());
+
+            stackOfStrings.AddRange(new string[] { "aaa", "bbb", "ccc" });
+            Console.WriteLine(stackOfStrings.IsEmpty());
+
+            Console.WriteLine(stackOfStrings.Pop());
+            Console.WriteLine(stackOfStrings.Pop());
+            Console.WriteLine(stackOfStrings.Pop());
+        }
+    }
+}
