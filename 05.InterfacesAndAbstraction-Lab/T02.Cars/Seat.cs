8e1944448f8c1d2dd18b63d14db63086297db00a
Aleksandar Dimitrov <alexkdimitrov@gmail.com>
2022-04-04 06:09:18 +0300

    Added Interfaces And Abstraction - Lab

---
+++ 05.InterfacesAndAbstraction-Lab/T02.Cars/Seat.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+
+        public string Model { get; set; }
+
+        public string Color { get; set; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{Color} Seat {Model}");
+            sb.AppendLine(this.Start());
+            sb.AppendLine(this.Stop());
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
