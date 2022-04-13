7d486c850601ac5a3628a36aed7c98dcf0e7c82b
Aleksandar Dimitrov <alexkdimitrov@gmail.com>
2022-04-13 23:59:38 +0300

    Added Reflection And Attributes - Lab

---
+++ 12.ReflectionAndAttributes-Lab/T01.Stealer/Spy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    class Spy
+    {
+        public string StealFieldInfo(string className,params string[] fieldNames)
+        {
+            StringBuilder result = new StringBuilder();
+            Type type = Type.GetType(className);
+            FieldInfo[] fields = type.GetFields((BindingFlags)60);
+
+            Console.WriteLine($"Class under investigation: {className}");
+            foreach (var field in fields)
+            {
+
+                if (fieldNames.Contains(field.Name))
+                {
+                    result.AppendLine($"{field.Name} = {field.GetValue(field.Name)}");
+                }
+            }
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
