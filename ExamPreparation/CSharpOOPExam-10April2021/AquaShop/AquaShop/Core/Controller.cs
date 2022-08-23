2aca53832bacb7fd569312cb017985b9d9d0d675
Aleksandar Dimitrov <alexkdimitrov@gmail.com>
2022-08-23 22:59:24 +0300

    Added exam prep

---
+++ ExamPreparation/CSharpOOPExam-10April2021/AquaShop/AquaShop/Core/Controller.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AquaShop.Core.Contracts;
+using AquaShop.Models.Aquariums;
+using AquaShop.Models.Aquariums.Contracts;
+using AquaShop.Models.Decorations;
+using AquaShop.Models.Decorations.Contracts;
+using AquaShop.Models.Fish;
+using AquaShop.Models.Fish.Contracts;
+using AquaShop.Repositories;
+using AquaShop.Repositories.Contracts;
+using AquaShop.Utilities.Messages;
+
+namespace AquaShop.Core
+{
+    public class Controller : IController
+    {
+        private readonly IRepository<IDecoration> decorations;
+        private readonly ICollection<IAquarium> aquariums;
+
+        public Controller()
+        {
+            this.decorations = new DecorationRepository();
+            this.aquariums = new List<IAquarium>();
+        }
+
+        public string AddAquarium(string aquariumType, string aquariumName)
+        {
+            IAquarium aquarium;
+            if (aquariumType == nameof(FreshwaterAquarium))
+            {
+                aquarium = new FreshwaterAquarium(aquariumName);
+            }
+            else if (aquariumType == nameof(SaltwaterAquarium))
+            {
+                aquarium = new SaltwaterAquarium(aquariumName);
+            }
+            else
+            {
+                throw new InvalidOperationException(ExceptionMessages.InvalidAquariumType);
+            }
+
+            this.aquariums.Add(aquarium);
+
+            return string.Format(OutputMessages.SuccessfullyAdded, aquariumType);
+        }
+
+        public string AddDecoration(string decorationType)
+        {
+            IDecoration decoration;
+            if (decorationType == nameof(Ornament))
+            {
+                decoration = new Ornament();
+            }
+            else if (decorationType == nameof(Plant))
+            {
+                decoration = new Plant();
+            }
+            else
+            {
+                throw new InvalidOperationException(ExceptionMessages.InvalidDecorationType);
+            }
+
+            this.decorations.Add(decoration);
+
+            return string.Format(OutputMessages.SuccessfullyAdded, decorationType);
+        }
+
+        public string InsertDecoration(string aquariumName, string decorationType)
+        {
+            IDecoration decoration = this.decorations.FindByType(decorationType);
+            if (decoration == null)
+            {
+                throw new InvalidOperationException(string.Format(ExceptionMessages.InexistentDecoration, decorationType));
+            }
+
+            IAquarium aquarium = this.aquariums.FirstOrDefault(a => a.Name == aquariumName);
+
+            aquarium.AddDecoration(decoration);
+            this.decorations.Remove(decoration);
+
+            return string.Format(OutputMessages.EntityAddedToAquarium, decorationType, aquariumName);
+        }
+
+        public string AddFish(string aquariumName, string fishType, string fishName, string fishSpecies, decimal price)
+        {
+            IFish fish;
+            if (fishType == nameof(FreshwaterFish))
+            {
+                fish = new FreshwaterFish(fishName, fishSpecies, price);
+            }
+            else if (fishType == nameof(SaltwaterFish))
+            {
+                fish = new SaltwaterFish(fishName, fishSpecies, price);
+            }
+            else
+            {
+                throw new InvalidOperationException(ExceptionMessages.InvalidFishType);
+            }
+
+            IAquarium aquarium = this.aquariums.FirstOrDefault(a => a.Name == aquariumName);
+            string aquariumType = aquarium.GetType().Name;
+
+            string fishWaterType = fishType.Substring(0, fishType.Length - 4);
+            string aquariumWaterType = aquariumType.Substring(0, aquariumType.Length - 8);
+
+            if (fishWaterType != aquariumWaterType)
+            {
+                return OutputMessages.UnsuitableWater;
+            }
+
+            aquarium.AddFish(fish);
+
+            return string.Format(OutputMessages.EntityAddedToAquarium, fishType, aquariumName);
+        }
+
+        public string FeedFish(string aquariumName)
+        {
+            IAquarium aquarium = this.aquariums.FirstOrDefault(a => a.Name == aquariumName);
+
+            var count = aquarium.Fish.Count;
+            aquarium.Feed();
+
+            return string.Format(OutputMessages.FishFed, count);
+        }
+
+        public string CalculateValue(string aquariumName)
+        {
+            IAquarium aquarium = this.aquariums.FirstOrDefault(a => a.Name == aquariumName);
+
+            var value = aquarium.Fish.Sum(x => x.Price);
+            value += aquarium.Decorations.Sum(x => x.Price);
+
+            return string.Format(OutputMessages.AquariumValue, aquariumName, value);
+        }
+
+        public string Report()
+        {
+            var sb = new StringBuilder();
+            foreach (var aquarium in this.aquariums)
+            {
+                sb.AppendLine(aquarium.GetInfo());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ ExamPreparation/CSharpOOPExam-10April2021/Aquariums/Aquariums.Tests/AquariumsTests.cs
@@ -0,0 +1,115 @@
+﻿namespace Aquariums.Tests
+{
+    using System;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class AquariumsTests
+    {
+        [Test]
+        public void FishConstructorShouldWorkProperly()
+        {
+            Fish fish = new Fish("Nemo");
+
+            Assert.AreEqual("Nemo", fish.Name);
+            Assert.IsTrue(fish.Available);
+        }
+
+        [Test]
+        public void AquariumConstructorShouldInitializeProperly()
+        {
+            Aquarium aquarium = new Aquarium("Big", 10);
+
+            Assert.AreEqual("Big", aquarium.Name);
+            Assert.AreEqual(10, aquarium.Capacity);
+            Assert.AreEqual(0, aquarium.Count);
+        }
+
+        [Test]
+        public void AquariumConstructorShouldThrowExceptionWihtInvlidName()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Aquarium("", 10));
+            Assert.Throws<ArgumentNullException>(() => new Aquarium(null, 10));
+        }
+
+        [Test]
+        public void AquariumConstructorShouldThrowExceptionWihNegativeCapacity()
+        {
+            Assert.Throws<ArgumentException>(() => new Aquarium("Big", -1));
+        }
+
+        [Test]
+        public void AddFishShouldWokrProperly()
+        {
+            Aquarium aquarium = new Aquarium("Big", 10);
+            Fish fish1 = new Fish("Nemo");
+            Fish fish2 = new Fish("Not Nemo");
+
+            aquarium.Add(fish1);
+            aquarium.Add(fish2);
+
+            Assert.AreEqual(2, aquarium.Count);
+        }
+
+        [Test]
+        public void AddFishShouldShouldThrowExceptionIfCapcityIsFull()
+        {
+            Aquarium aquarium = new Aquarium("Big", 1);
+            Fish fish1 = new Fish("Nemo");
+            Fish fish2 = new Fish("Not Nemo");
+
+            aquarium.Add(fish1);
+
+            Assert.Throws<InvalidOperationException>(() => aquarium.Add(fish2));
+        }
+
+        [Test]
+        public void SellFishShouldSetFishAvailabilityToFalseAndRetunIt()
+        {
+            Aquarium aquarium = new Aquarium("Big", 10);
+            Fish fish1 = new Fish("Nemo");
+            Fish fish2 = new Fish("Not Nemo");
+
+            aquarium.Add(fish1);
+            aquarium.Add(fish2);
+
+            var soldFish = aquarium.SellFish("Nemo");
+
+            Assert.AreEqual(fish1, soldFish);
+            Assert.IsFalse(soldFish.Available);
+        }
+
+        [Test]
+        public void SellFishShouldThrowExceptionIfFishNotExist()
+        {
+            Aquarium aquarium = new Aquarium("Big", 10);
+            Fish fish1 = new Fish("Nemo");
+            Fish fish2 = new Fish("Not Nemo");
+
+            aquarium.Add(fish1);
+            aquarium.Add(fish2);
+
+            Assert.Throws<InvalidOperationException>(() => aquarium.SellFish("Gogo"));
+        }
+
+        [Test]
+        public void ReportShouldReturnAppropriateResult()
+        {
+            Aquarium aquarium = new Aquarium("Big", 10);
+            Fish fish1 = new Fish("Nemo");
+            Fish fish2 = new Fish("Not Nemo");
+            Fish fish3 = new Fish("Nemo2");
+
+            aquarium.Add(fish1);
+            aquarium.Add(fish2);
+            aquarium.Add(fish3);
+
+            var fishNames = $"{fish1.Name}, {fish2.Name}, {fish3.Name}";
+            var expectedResult = $"Fish available at {aquarium.Name}: {fishNames}";
+
+            var actualtResult = aquarium.Report();
+
+            Assert.AreEqual(expectedResult, actualtResult);
+        }
+    }
+}
